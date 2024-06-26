ea22d94afcbbfa68eff2354f74625b7336d04f83
Nuelo <akalo.emmanuel18@gmail.com>
2024-06-26 07:08:36 +0100

    feat: implement creation of instance using kwarg passed as dict

--- models/base_model.py
+++ models/base_model.py
@@ -13,14 +13,21 @@ class BaseModel:
     BaseModel class
     This class defines common attributes and methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, **kwargs):
         """
         Initializes a new BaseModel instance.
         Sets the id, created_at, and updated_at attributes.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = dt.datetime.now()
-        self.updated_at = dt.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key  in ['created_at', 'updated_at']:
+                        value = dt.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = dt.datetime.now()
+            self.updated_at = dt.datetime.now()
 
     def __str__(self):
         """
